1b6e1151ebebe7c56b636346fee344fb0804c81f
Nicholas Fung <nbfung@sfu.ca>
2017-10-16 11:59:52 -0700

    Initial Commit

---
+++ ImageGallery/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageGallery
+//
+//  Created by Nicholas Fung on 2017-10-16.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
